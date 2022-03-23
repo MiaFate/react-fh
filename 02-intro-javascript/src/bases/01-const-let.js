642b8ea6b197d5855199e4aecb2554b76d6b5f80
MiaFate <miapurplefroot@gmail.com>
2022-03-23 20:43:32 -0300

    clase 13

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,14 @@
+//variable
+
+const nombre = "Mia";
+let apellido = "Ramos";
+
+let valorDado = 5;
+valorDado = 4;
+console.log(nombre, apellido, valorDado);
+
+if (true) {
+    let nombre = "Peter";
+    console.log(nombre);
+}
+console.log(valorDado);
