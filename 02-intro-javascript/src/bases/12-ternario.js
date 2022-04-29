1b7560d052958843cce333bcbdc8dc4e331b9533
MiaFate <miapurplefroot@gmail.com>
2022-04-29 13:30:52 -0300

    ternario

---
+++ 02-intro-javascript/src/bases/12-ternario.js
@@ -0,0 +1,12 @@
+const activo = true
+/*let mensaje = ''
+
+ if(!activo){
+    mensaje = 'activo'
+}else{
+    mensaje = 'inactivo'
+} */
+
+//const mensaje =(activo) ? 'activo' : 'inactivo'
+const mensaje = !activo && 'Activo'
+console.log(mensaje)
